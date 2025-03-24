c2008c56ecac5edaf8096f4c0a4581647cef92ba
xuemian <xuemian888@gmail.com>
2025-03-24 21:18:53 +1000

    chore: update axios baseURL to use /api

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://52.65.208.68:5001', // live
+  baseURL: 'http://52.65.208.68/api', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
